3bad28523bc8055a540094c553f4b6d29b2b5684
Vedant Kherade <152008775+vedantkherade@users.noreply.github.com>
2025-10-29 22:50:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function message(){
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve("Hello world!");
+		},1000)
+	})
+}
+
+message().then((msg) => {
+	document.getElementById("output").textContent = msg;
+})
